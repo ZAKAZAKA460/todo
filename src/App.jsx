52cf11a811f06161ba8a28d34540de3a18a58104
ZAKAZAKA460 <92558285+ZAKAZAKA460@users.noreply.github.com>
2024-09-17 19:20:19 +1000

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './components/layouts/header';
+import Main from './components/layouts/main';
+
+function App() {
+  return (<>
+    <Header/>
+    <Main />
+    <h1>TODO</h1>
+  </>)
+}
+
+export default App
---
+++ src/components/layouts/main.jsx
@@ -0,0 +1,10 @@
+import iti from "../../assets/images/iti.png";
+
+export default function Main(){
+    console.log(iti);
+    return (
+        <main>
+            <img src={iti} alt="" />
+        </main>
+    );
+}
