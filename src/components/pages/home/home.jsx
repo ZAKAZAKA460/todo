12fcb78da9e1eb1c7ff32238a768865128d02a62
ZAKAZAKA460 <22andricer@gmail.com>
2024-10-22 19:15:49 +1000

    сфкв

--- src/components/pages/home/home.jsx
+++ src/components/pages/home/home.jsx
@@ -1,33 +1,38 @@
-import { Button, ButtonIcon } from "@/components/shared/UI/buttons/buttons.jsx";
-import sun from "@/assets/icons/sun.svg"
+
 import Header from "@/components/shared/header/header.jsx";
 import Todocard from "@/components/shared/card/todo";
 
-const card ={
-    title:"New oijoijoi",
+const cardMass =[
+{
+    title:"New project",
     project:"task",
     status:"In progress",
-    progress:10,
+    progress:100,
     dueDate: "01.09.22"
-}
+},
+{
+    title:"Buy bread",
+    project:"Goal",
+    status:"In progress",
+    progress:79,
+    dueDate: "15.10.24"
+},
+
+];
+
 export default function Home(){
     return (
         <main>
-            <h1>home</h1>
-            <ButtonIcon icon={sun}/>
-            <Button 
-                styles={{ 
-                backgroundColor: "black",
-                color: "white",
-                borderRadious: "19px",
-                outline: "none",
-                border: "none",
-                padding: "12px 24px",
-            }}
-            child={"New Template"}>
-            </Button>
+            
             <Header />
-            <Todocard card={card} />
+            
+            
+                {cardMass.map((card,index) => {
+                  return ( <Todocard card={card} />)
+                   
+                
+            })}
+
         </main>
     );
 }
--- src/components/shared/card/todo.jsx
+++ src/components/shared/card/todo.jsx
@@ -1,26 +1,33 @@
 import { ButtonIcon } from "@/components/shared/UI/buttons/buttons.jsx";
 import progress from "@assets/icons/progress.svg"
-import bar from "@assets/icons/bar.svg"
 import menu from "@assets/icons/menu.svg"
 import style from "./style.module.css"
 
 const Todocard = ({card}) =>{
-    return <article className={style.card}>
+    return (<article className={style.card}>
         <div className={style.top}>
         <h1>{card.title}</h1>
         <ButtonIcon icon={menu} />
         </div>
+        
         <div className={style.title}>
         <ButtonIcon icon={progress} />
         <p>{card.status}</p> 
-        <p>{card.progress}/10</p>
+        <p>{card.progress}/100</p>
         </div>
-        <ButtonIcon icon={bar} />
+        <div className={style.progress}>
+            <div style={{
+                width: card.progress + "%",
+                backgroundColor: card.progress <= 40 ? ' rgb(255, 0, 0)': card.progress <= 99 ? 'rgb(255, 136, 0)': 'rgb(0, 255, 76)',
+            }} className={style.progress_bar}></div>
+        </div>
         <div className={style.dateBlock}>
         <p>{card.dueDate}</p>
         </div>
         
     </article>
+    );
 };
 
 export default Todocard;
+
